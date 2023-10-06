f2934451b89ccc03d233f2211f7e8d22ada7b5c5
Ryan Bott <bott.ryan@gmail.com>
2023-10-05 18:40:45 -0600

    added color to Shape

--- app/src/main/java/com/example/lab4/Shape.java
+++ app/src/main/java/com/example/lab4/Shape.java
@@ -3,6 +3,7 @@ package com.example.lab4;
 public abstract class  Shape {
     public int x;
     public int y;
+    String color = "blue";
     public Shape(int x, int y) {
         this.x= x;
         this.y = y;
