c08912aa720e91bb3708360c509b35550bf54e24
Maksimus Andruchow <maksimus@ualberta.ca>
2023-10-05 19:30:42 -0600

    conflicting

--- app/src/main/java/com/example/lab4/Shape.java
+++ app/src/main/java/com/example/lab4/Shape.java
@@ -3,8 +3,10 @@ package com.example.lab4;
 public abstract class  Shape {
     public int x;
     public int y;
+    String color;
     public Shape(int x, int y) {
         this.x= x;
         this.y = y;
+	this.color = "Red";
     }
 }
