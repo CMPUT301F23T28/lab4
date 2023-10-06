5ff60fb7591686aeea7d558468580e3df4b0b0ee
Craig Lobo <craiglobo1@gmail.com>
2023-10-05 18:34:28 -0600

    added Shape Class

---
+++ app/src/main/java/com/example/lab4/Shape.java
@@ -0,0 +1,10 @@
+package com.example.lab4;
+
+public abstract class  Shape {
+    public int x;
+    public int y;
+    public Shape(int x, int y) {
+        this.x= x;
+        this.y = y;
+    }
+}
