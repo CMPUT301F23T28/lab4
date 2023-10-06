83fbf3cc8e56f0b73f8ea3cf771c94bf422aed57
Jordzira <69765875+Jordzira@users.noreply.github.com>
2023-10-05 18:52:28 -0600

    color

--- app/src/main/java/com/example/lab4/Parallelepiped.java
+++ app/src/main/java/com/example/lab4/Parallelepiped.java
@@ -8,5 +8,5 @@ public class Parallelepiped extends Shape {
         this.x=x;
         this.y= y;
     }
-    
+
 }
--- app/src/main/java/com/example/lab4/Shape.java
+++ app/src/main/java/com/example/lab4/Shape.java
@@ -3,6 +3,8 @@ package com.example.lab4;
 public abstract class  Shape {
     public int x;
     public int y;
+    String color = "blue";
+
     public Shape(int x, int y) {
         this.x= x;
         this.y = y;
